75994951db060791b4387c7bc4458a75a7e02afb
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-05-18 22:52:56 +0300

    success

---
+++ MinimalApi/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace MinimalApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("this is a controller response");
+        }
+    }
+}
--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,10 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
 var app=builder.Build();
 app.UseHttpsRedirection();
+app.MapControllers();
 app.MapGet("/tests", () => "hello world");
 app.MapGet("test2", () => "alican");
+app.MapGet("test5", (int age) =>
+{
+    return "hello world" +  age;
+});
 app.MapGet("/get",() => "this is a get request");
 app.MapPost("/post",() => "this is a post request");
 app.MapPut("/put",() => "this is a put request");
 app.MapDelete("/delete",() => "this is a delete request");
+app.MapGet("/return-result", () =>
+{
+    return Results.Ok(new { Message = "Api is working" });
+});
 app.Run();
